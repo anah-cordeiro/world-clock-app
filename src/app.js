45aa664bee489b908ac08c9db4afa695cb216e7f
Rvnes <ana_reis_cordeiro@hotmail.com>
2023-03-28 22:13:32 +0100

    Updated HTML, CSS & JS

--- src/app.js
+++ src/app.js
@@ -1,25 +1,40 @@
-function showSelectedCityDate(event) {
-  if (event.target.value.length > 0) {
-    let currentDate = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY H:mA");
-    alert(`It is ${currentDate} in ${event.target.value}`);
+function defaultTime() {
+  //Washington DC
+  let vancouverElement = document.querySelector("#vancouver");
+
+  if (vancouverElement) {
+    let vancouverDateElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTime = moment().tz("America/Vancouver");
+
+    vancouverDateElement.innerHTML = vancouverTime.format("MMMM     Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTime.format("h:mm:ss A");
+  }
+
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
+
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM     Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format("h:mm:ss A");
+  }
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM     Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
   }
 }
 
-let citySelect = document.querySelector("#cityTime");
-
-citySelect.addEventListener("change", showSelectedCityDate);
-
-function displayTime() {
-  let date = document.querySelector("#date");
-
-  date.innerHTML = new Date();
-}
-
-setInterval(displayTime, 1000);
-
-let honoluluTime = moment()
-  .tz("Pacific/Honululu")
-  .format("dddd, MMMM D, YYYY H:mA");
-let honululuElement = document.querySelector("#");
+defaultTime();
+setInterval(defaultTime, 1000);
